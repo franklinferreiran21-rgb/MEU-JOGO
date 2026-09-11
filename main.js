f995d31e5c219e2e36e235607d813c7ac610bf4e
franklinferreiran21-rgb <franklinferreiran21@gmail.com>
2026-09-10 22:24:24 -0300

    Criar main.js para organizar lógica do jogo

---
+++ main.js
@@ -0,0 +1,39 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
+import { criarMapa } from './mapas/mapa1.js';
+import { criarPlayer } from './player/player.js';
+import { criarJoystick } from './ui/joystick.js';
+import { criarCameraRotativa } from './ui/cam.js';
+
+const cena = new THREE.Scene();
+cena.background = new THREE.Color(0x87ceeb);
+
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+
+cena.add(new THREE.DirectionalLight(0xffffff,2));
+cena.add(new THREE.AmbientLight(0xffffff,0.5));
+
+cena.add(criarMapa());
+
+const player = criarPlayer();
+cena.add(player);
+
+const joystick = criarJoystick();
+const cam = criarCameraRotativa(camera, player);
+
+function animar(){
+ requestAnimationFrame(animar);
+ player.atualizar(joystick);
+ cam.atualizar();
+ renderer.render(cena,camera);
+}
+animar();
+
+addEventListener('resize',()=>{
+ renderer.setSize(innerWidth, innerHeight);
+ camera.aspect = innerWidth / innerHeight;
+ camera.updateProjectionMatrix();
+});
